e8017d0924a24219b4345a6ab33572bb14bbf135
hongjiliang <chkimleang@gmail.com>
2017-10-08 14:00:45 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Update
                 </div>
 
                 <div class="links">
